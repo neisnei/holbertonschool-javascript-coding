118d1c30b9d6c272740c07f2081432ced294a4db
neisnei <neischalyruidiaz@gmail.com>
2024-01-24 22:14:09 +0000

    README.md

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -1,14 +1,23 @@
 #!/usr/bin/node
 
-def read_file(file_path):
-  """Reads the contents of a file and prints it to the console.
+const fs = require('fs');
 
-  Args:
-    file_path: Path to the file to be read.
-  """
-  try:
-    with open(file_path, "r", encoding="utf-8") as file:
-      content = file.read()
-      print(content)
-  except Exception as error:
-    print(error)
+// Check if the file path is provided as an argument
+if (process.argv.length !== 3) {
+  console.error('Usage: node 0-readme.js <file-path>');
+  process.exit(1); // Exit with an error code
+}
+
+const filePath = process.argv[2];
+
+// Read the content of the file in utf-8
+fs.readFile(filePath, 'utf-8', (err, data) => {
+  if (err) {
+    // Print the error object if an error occurred during reading
+    console.error(err);
+    process.exit(1); // Exit with an error code
+  } else {
+    // Print the content of the file
+    console.log(data);
+  }
+});
