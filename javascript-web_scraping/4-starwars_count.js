8dd80dac40f2057710f52758f3bebf0ce248f853
neisnei <neischalyruidiaz@gmail.com>
2024-01-24 22:31:00 +0000

    star

---
+++ javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,24 @@
+#!/usr/bin/node
+/*
+    prints the number of movies where the
+    character “Wedge Antilles” is present.
+*/
+const request = require('request');
+const url = process.argv[2];
+
+request(url, function (error, response, body) {
+  if (error) {
+    console.log('code:', response.statusCode);
+  } else {
+    const jsonBody = JSON.parse(body);
+    let count = 0;
+    for (const film of jsonBody.results) {
+      for (const character of film.characters) {
+        if (character.endsWith('/18/')) { // ...people/18 is Wedge Antilles
+          count += 1;
+        }
+      }
+    }
+    console.log(count);
+  }
+});
