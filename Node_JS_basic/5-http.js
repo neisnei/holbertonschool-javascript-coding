171c1e3f9c5f4c7213d5cae68de87940fc6acb24
neisnei <neischalyruidiaz@gmail.com>
2024-02-02 14:10:04 +0000

    https

--- Node_JS_basic/5-http.js
+++ Node_JS_basic/5-http.js
@@ -1,20 +1,28 @@
 const http = require('http');
-const countStudents = require('./3-read_file_async');
+const fs = require('fs');
 
-const app = http.createServer(async (req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  if (req.url === '/') res.write('Hello Holberton School!');
-  if (req.url === '/students') {
-    res.write('This is the list of our students\n');
-    try {
-      const data = await countStudents(process.argv[2]);
-      res.end(`${data.join('\n')}`);
-    } catch (error) {
-      res.end(error.message);
-    }
+const app = http.createServer((req, res) => {
+  const url = req.url;
+  if (url === '/') {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('Hello Holberton School!');
+  } else if (url === '/students') {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    const fileName = process.argv[2];
+    fs.readFile(fileName, 'utf8', (err, data) => {
+      if (err) {
+        res.end('Error reading file');
+      } else {
+        const students = data.split('\n').filter(line => line !== '');
+        res.end(`This is the list of our students:\n${students.join('\n')}`);
+      }
+    });
+  } else {
+    res.writeHead(404, { 'Content-Type': 'text/plain' });
+    res.end('Not found');
   }
-  res.end();
 });
+
 app.listen(1245);
+
 module.exports = app;
