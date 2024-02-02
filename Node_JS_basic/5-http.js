b261cbb7637104e127bc0f83bb6da27a04094e92
neisnei <neischalyruidiaz@gmail.com>
2024-02-02 15:25:48 +0000

    http js

--- Node_JS_basic/5-http.js
+++ Node_JS_basic/5-http.js
@@ -1,20 +1,34 @@
 const http = require('http');
+
 const countStudents = require('./3-read_file_async');
+
+const args = process.argv.slice(2);
+
+const hostname = '127.0.0.1';
+const port = 1245;
 
 const app = http.createServer(async (req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  if (req.url === '/') res.write('Hello Holberton School!');
-  if (req.url === '/students') {
+
+  const { url } = req;
+
+  if (url === '/') res.end('Hello Holberton School!');
+  else if (url === '/students') {
     res.write('This is the list of our students\n');
     try {
-      const data = await countStudents(process.argv[2]);
-      res.end(`${data.join('\n')}`);
+      const students = await countStudents(args.toString());
+      res.end(`${students.join('\n')}`);
     } catch (error) {
       res.end(error.message);
     }
   }
+  res.statusCode = 404;
   res.end();
 });
-app.listen(1245);
+
+app.listen(port, hostname, () => {
+  // console.log(`Server running at http://${hostname}:${port}/`);
+});
+
 module.exports = app;
